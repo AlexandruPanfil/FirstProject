f9083dbff6d4307d84cb4dded42cf6a60a182216
AlexandruPanfil <111560446+AlexandruPanfil@users.noreply.github.com>
2024-02-17 21:52:19 +0200

    added some buttons, also added counter for table id

--- View/work_interface.py
+++ View/work_interface.py
@@ -22,6 +22,7 @@ class Root_work(Tk):
         # self.menu_option.add_separator()
         # self.menu_option.add_command(label="Exit", command=self.destroy)
         # self.menu_bar.add_cascade(label="Options", menu=self.menu_option)
+        self.count = []
 
         self.produs_code_message = Message(self, text="ID: ", font=self.font_bold, fg="black")
         self.produs_code_message.place(x=10, y=10, width=120, height=40)
@@ -75,7 +76,7 @@ class Root_work(Tk):
         self.scroll_bar.grid(row=0, column=1)
 
         self.line2 = Label(self, bg="gray")
-        self.line2.place(x=10, y=714, width=850, height=600)
+        self.line2.place(x=10, y=715, width=850, height=600)
 
         self.total_message = Message(self, text="Total: ", font=self.font_bold)
         self.total_message.place(x=10, y=715, width=100, height=40)
@@ -84,61 +85,76 @@ class Root_work(Tk):
         self.total_label.place(x=110, y=715, width=750, height=40)
 
         self.white_label = Label(self)
-        self.white_label.place(x=885, y=115, width=635, height=600)
+        self.white_label.place(x=885, y=110, width=635, height=640)
 
         self.clear_button = Button(self, text="Clear", font=self.font_bold, command=self.clear)
-        self.clear_button.place(x=885, y=115, width=100, height=80)
+        self.clear_button.place(x=885, y=120, width=100, height=80)
 
         self.add_entry = Entry(self, text="", font=self.font_bold, justify="center")
-        self.add_entry.place(x=985, y=115, width=535, height=80)
+        self.add_entry.place(x=995, y=120, width=515, height=80)
 
         self.button_7 = Button(self, text=7, command=self.button_7, font=self.font_bold, bg="light blue")
-        self.button_7.place(x=885, y=200, width=100, height=80)
+        self.button_7.place(x=885, y=210, width=100, height=80)
 
         self.button_8 = Button(self, text=8, command=self.button_8, font=self.font_bold, bg="light blue")
-        self.button_8.place(x=995, y=200, width=100, height=80)
+        self.button_8.place(x=995, y=210, width=100, height=80)
 
         self.button_9 = Button(self, text=9, command=self.button_9, font=self.font_bold, bg="light blue")
-        self.button_9.place(x=1105, y=200, width=100, height=80)
+        self.button_9.place(x=1105, y=210, width=100, height=80)
 
         self.button_4 = Button(self, text=4, command=self.button_4, font=self.font_bold, bg="light blue")
-        self.button_4.place(x=885, y=290, width=100, height=80)
+        self.button_4.place(x=885, y=300, width=100, height=80)
 
         self.button_5 = Button(self, text=5, command=self.button_5, font=self.font_bold, bg="light blue")
-        self.button_5.place(x=995, y=290, width=100, height=80)
+        self.button_5.place(x=995, y=300, width=100, height=80)
 
         self.button_6 = Button(self, text=6, command=self.button_6, font=self.font_bold, bg="light blue")
-        self.button_6.place(x=1105, y=290, width=100, height=80)
+        self.button_6.place(x=1105, y=300, width=100, height=80)
 
         self.button_1 = Button(self, text=1, command=self.button_1, font=self.font_bold, bg="light blue")
-        self.button_1.place(x=885, y=380, width=100, height=80)
+        self.button_1.place(x=885, y=390, width=100, height=80)
 
         self.button_2 = Button(self, text=2, command=self.button_2, font=self.font_bold, bg="light blue")
-        self.button_2.place(x=995, y=380, width=100, height=80)
+        self.button_2.place(x=995, y=390, width=100, height=80)
 
         self.button_3 = Button(self, text=3, command=self.button_3, font=self.font_bold, bg="light blue")
-        self.button_3.place(x=1105, y=380, width=100, height=80)
+        self.button_3.place(x=1105, y=390, width=100, height=80)
 
         self.search_button = Button(self,text="Search", command=self.search, font=self.font_bold, bg="light blue")
-        self.search_button.place(x=885, y=470, width=100, height=80)
+        self.search_button.place(x=885, y=480, width=100, height=80)
 
         self.button_0 = Button(self, text=0, command=self.button_0, font=self.font_bold, bg="light blue")
-        self.button_0.place(x=995, y=470, width=100, height=80)
+        self.button_0.place(x=995, y=480, width=100, height=80)
 
         self.dot_button = Button(self,text=".", command=self.dot, font=self.font_bold, bg="light blue")
-        self.dot_button.place(x=1105, y=470, width=100, height=80)
+        self.dot_button.place(x=1105, y=480, width=100, height=80)
 
         self.add_button = Button(self,text="Add", font=self.font_bold, bg="light green", command=self.add)
-        self.add_button.place(x=1215, y=200, width=300, height=80)
+        self.add_button.place(x=1215, y=210, width=300, height=80)
 
         self.delete_button = Button(self,text="Delete", font=self.font_bold, bg="light green", command=self.delete)
-        self.delete_button.place(x=1215, y=290, width=300, height=80)
+        self.delete_button.place(x=1215, y=300, width=300, height=80)
 
         self.anulare_button = Button(self,text="Anulare", font=self.font_bold, bg="light green", command=self.anulare)
-        self.anulare_button.place(x=1215, y=380, width=300, height=80)
+        self.anulare_button.place(x=1215, y=390, width=300, height=80)
 
         self.pachet_button = Button(self,text="Pachet", font=self.font_bold, bg="light blue", command=self.pachet)
-        self.pachet_button.place(x=1215, y=470, width=300, height=80)
+        self.pachet_button.place(x=1215, y=480, width=300, height=80)
+
+        self.achitat_label = Label(self, text="Achitat", font=self.font_bold,)
+        self.achitat_label.place(x=885, y=570, width=100, height=80)
+
+        self.achitat_entry = Entry(self, text=0.0, font=self.font_bold, justify="center")
+        self.achitat_entry.place(x=995, y=570, width=515, height=80)
+
+        self.discount = Button(self, text="Discount", font=self.font_bold, justify="center", command=self.discount, bg="light pink")
+        self.discount.place(x=885, y=660, width=200, height=80)
+
+        self.achitat_card = Button(self, text="Card", font=self.font_bold, justify="center", command=self.card, bg="light pink")
+        self.achitat_card.place(x=1098, y=660, width=200, height=80)
+
+        self.achitat_cash = Button(self, text="Cash", font=self.font_bold, justify="center", command=self.cash, bg="light pink")
+        self.achitat_cash.place(x=1311, y=660, width=200, height=80)
 
     def search(self):
         print("Hello")
@@ -173,36 +189,26 @@ class Root_work(Tk):
     def add(self):
         bar_code = int(self.add_entry.get())
         self.product = products_db.get_from_db_Products(bar_code)
-        # print(product)
-        # self.date_tabel = []
-        # self.date_tabel.append(product)
         bucati_produs = Bucati(self.callback_bucati)
-        # print(bucati_produs)
-        # # Utilizează un index pentru a itera prin listă dacă ai nevoie să modifici elementele
-        # for index, (id, nume, pret, buc, pret_total) in enumerate(self.date_tabel):
-        #     id = index + 1
-        #     buc = 1
-        #     pret_total = pret * buc
-        #
-        #     self.date_tabel[index] = (id, nume, pret, buc, pret_total)
-        #
-        # for date in self.date_tabel:
-        #     self.tabel_view.insert("", END, values=date)
         return self.product
+
 
     def callback_bucati(self, valoare):
         self.date_tabel = []
         self.date_tabel.append(self.product)
-        # print(self.date_tabel)
         self.clear()
         for index, (id, nume, pret, buc, pret_total) in enumerate(self.date_tabel):
-            id = id
+            self.count.append(id)
+            id = len(self.count)
             buc = valoare
             pret_total = pret * buc
             self.date_tabel[index] = (id, nume, pret, buc, pret_total)
 
+
         for date in self.date_tabel:
             self.tabel_view.insert("", END, values=date)
+
+        # TODO This part of code, if the intem is duplicating to add more quantity
         # for index, (id, nume, pret, buc, pret_total) in enumerate(self.date_tabel):
         #     if index
         #             buc += 1
@@ -221,10 +227,29 @@ class Root_work(Tk):
         self.produs_code_label["text"] = ""
 
     def pachet(self):
-        pass
+        product = products_db.get_from_db_Products(987987)
+        self.date_tabel = []
+        self.date_tabel.append(product)
+
+        for index, (id, nume, pret, buc, pret_total) in enumerate(self.date_tabel):
+            self.count.append(id)
+            id = len(self.count)
+            buc = 1
+            pret_total = pret * 1
+            self.date_tabel[index] = (id, nume, pret, buc, pret_total)
+
+        for date in self.date_tabel:
+            self.tabel_view.insert("", END, values=date)
+
+    def discount(self):
+        print("Discount 5%")
+    def card(self):
+        print("Achitat cu card, summa xxx")
+    def cash(self):
+        print("Achitat cu cash, summa xxx")
 
 
 
-# if __name__ == "__main__":
-#     root2 = Root_work()
-#     root2.mainloop()
+if __name__ == "__main__":
+    root2 = Root_work()
+    root2.mainloop()
